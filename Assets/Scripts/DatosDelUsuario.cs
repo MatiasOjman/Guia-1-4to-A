d8ba6ada6cb01467508500c5bd56e0f82d3d5acb
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2025-03-25 12:12:55 -0300

    empezamos punto 2

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float estaturaUsuario;
+    public bool donante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("mi nombre es" + nombreUsuario);
+        Debug.Log("tengo" + edadUsuario + "años");
+        Debug.Log("mido" + estaturaUsuario + "metros");
+        Debug.Log("donante" + donante);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
